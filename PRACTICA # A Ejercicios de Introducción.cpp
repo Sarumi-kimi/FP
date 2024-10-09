6b5e5d934b8e774822e501bfb2bc854770609917
Sarumi-kimi <kimi.rosas35@my.unitec.edu.mx>
2024-10-09 14:00:37 -0600

    Create PRACTICA # A Ejercicios de Introducción.cpp

---
+++ PRACTICA # A Ejercicios de Introducción.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    //Materias que estoy cursando
+        int numeroDeMaterias = 5; 
+        std::cout << "Número de materias que estoy cursando: " << numeroDeMaterias << std::endl;
+
+    // Cuatrimestre que estoy cursando
+        int cuatrimestre = 7; 
+        std::cout << "Cuatrimestre que estoy cursando: " << cuatrimestre << std::endl;
+
+    //Materia
+        std::string materia = "Programacion"; 
+        std::cout << "Materia: " << materia << std::endl;
+
+    //Numero de cuenta
+        std::string cuenta = "335010224"; 
+        std::cout << "Número de cuenta: " << cuenta << std::endl;
+
+    //Mi nombre
+        std::string nombre = "Kimi Sarumi"; 
+        std::cout << "Nombre: " << nombre << std::endl;
+
+    //Apellido Paterno
+        std::string apellidoPaterno = "Rosas";
+        std::cout << "Apellido Paterno: " << apellidoPaterno << std::endl;
+
+   //Apellido Materno
+        std::string apellidoMaterno = "Soto";
+        std::cout << "Apellido Materno: " << apellidoMaterno << std::endl;
+
+    //Celular
+        long celular = 5610858525;
+        std::cout << "Número de celular: " << celular << std::endl;
+
+    //Numero de casa
+        long casa = 76751876; 
+        std::cout << "Número de casa: " << casa << std::endl;
+
+    //Correo
+        std::string correoQueMasUso = "sarumikimi1502@gmail.com";  
+        std::cout << "Correo que más uso: " << correoQueMasUso << std::endl;
+
+    //Correo UNITEC
+        std::string correoMyUnitec = "kimi.rosas35@my.unitec.edu.mx";  
+        std::cout << "Correo MyUnitec: " << correoMyUnitec << std::endl;
+
+    //Carrera
+        std::string carrera = "Animación Digital";  
+        std::cout << "Carrera: " << carrera << std::endl;
+
+    //Blackboard
+        bool blackboard = true; 
+        std::cout << "¿Has usado Blackboard?: " << (blackboard ? "Sí" : "No") << std::endl;
+        return 0;
+    }
