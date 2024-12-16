dc75059b39adf8ac88a157a995c42367f308b51d
Sarumi-kimi <kimi.rosas35@my.unitec.edu.mx>
2024-12-16 11:26:56 -0600

    Create examenfinal.cpp

---
+++ examenfinal.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int impar; 
+    impar = (1, 3, 5, 9, 11, 13, 15, 17, 19, 23, 25, 27, 29, 31, 33, 37, 39, 41, 43, 45, 47, 51, 53, 55, 57, 59, 61, 65, 67, 69, 71, 73, 75, 79, 81, 83, 85, 87, 89, 93, 95, 97, 99);
+    cout << "Estos numeros son impares" << impar << endl;
+    
+
+    //2
+    int numero;
+    numero = (0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+    if (numero = 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+        cout << "Su numero está en el rango" << endl;
+    else
+        cout << "Su numero no está en el rango" << endl; 
+
+    //3
+    int impar;
+    impar = (1, 3, 5, 9, 11, 13, 15, 17, 19, 23, 25, 27, 29, 31, 33, 37, 39, 41, 43, 45, 47, 51, 53, 55, 57, 59, 61, 65, 67, 69, 71, 73, 75, 79, 81, 83, 85, 87, 89, 93, 95, 97, 99);
+    cout << "Ingrese un numero: ";
+    if (impar)
+        cout << "Su numero es impar" << endl;
+    else
+        cout << "Su numero es par" << endl;
+}
